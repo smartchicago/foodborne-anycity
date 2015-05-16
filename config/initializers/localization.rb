6cad94190b0830f52b863add85817afa94a55fbe
Nathan Green <nathangreen@NathanG2014-mac.local>
2015-05-16 13:49:51 -0500

    fix type in localization

--- config/initializers/localization.rb
+++ config/initializers/localization.rb
@@ -1,3 +1,3 @@
 # Load the localization file, or grab from ENV
 localization_file = "#{Rails.root}/config/localization.yml"
-L10N = File.exist?(localization) ? YAML.load_file(localization) : ENV
+L10N = File.exist?(localization_file) ? YAML.load_file(localization_file) : ENV
