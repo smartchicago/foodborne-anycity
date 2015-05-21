fb129144dd8f8828ce5198238bc7a0486be7d848
Nathan Green <nathangreen@NathanG2014-mac.local>
2015-05-20 21:50:14 -0500

    check to see if API is enabled before sending request

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -4,38 +4,48 @@ require "httparty"
 class Submission < ActiveRecord::Base
   # Attributes
   attr_accessible :restaurant_name, :restaurant_address, :description, :date,
-                  :first_name, :last_name, :twitter_handle, :email, :phone, :tweet_id
+    :first_name, :last_name, :twitter_handle, :email, :phone, :tweet_id
 
   # Validation
   validates :restaurant_name, :restaurant_address, :description, :date,
-            :first_name, :last_name, :email,
-            :presence => true
+    :first_name, :last_name, :email,
+    :presence => true
 
   # Callbacks
   before_create :create_service_request
 
-  private
-    # Create an Open311 service request
-    def create_service_request
-      # Geocode the address
-      lat, long = Geocoder.coordinates("#{self.restaurant_address} , #{L10N['CITY']}, #{L10N['STATE_ABBREV']}") 
+  def api_enabled?
+    L10N['API_ENABLED'] ? true : false
+  end
 
-      HTTParty.post('http://311api.cityofchicago.org/open311/v2/requests.json', :body => { 
-        :api_key => SETTINGS["OPEN_311_KEY"],
-        :service_code => '4fd6e4ece750840569000019',
-        :attribute => {
-            :PLEASESE => 'FOODPOIS',
-            :WHATISTH => self.restaurant_name,
-            :ONWHATDA => self.date
-        },
-        :address_string => self.restaurant_address,
-        :description => self.description,
-        :lat => lat, 
-        :long => long, 
-        :first_name => self.first_name,
-        :last_name => self.last_name,
-        :email => self.email,
-        :phone => self.phone
-      })
+  private
+
+  # Create an Open311 service request
+  def create_service_request
+    # Geocode the address
+    lat, long = Geocoder.coordinates("#{self.restaurant_address} , #{L10N['CITY']}, #{L10N['STATE_ABBREV']}") 
+    request_body =  {
+      :api_key => L10N['API_KEY'],
+      :service_code => L10N['API_SERVICE_CODE'],
+      :attribute => {
+      :PLEASESE => 'FOODPOIS',
+      :WHATISTH => self.restaurant_name,
+      :ONWHATDA => self.date
+    },
+      :address_string => self.restaurant_address,
+      :description => self.description,
+      :lat => lat, 
+      :long => long, 
+      :first_name => self.first_name,
+      :last_name => self.last_name,
+      :email => self.email,
+      :phone => self.phone
+    }
+
+    if api_enabled?
+      HTTParty.post(L10N['API_POST_ENDPOINT'], :body => request_body)
+    else
+      puts request_body
     end
+  end
 end
